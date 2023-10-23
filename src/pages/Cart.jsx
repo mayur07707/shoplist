7d247ad90f4f593dd870315f070dd462746b40e9
Mayur Shinde <mayurms2758@gmail.com>
2023-10-23 21:09:09 +0530

    final commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,8 +5,8 @@ export const Cart = () => {
   useTitle("Cart");
   
   const products = [
-    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
-    {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
+    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/images/1001.png",}, 
+    {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/images/1002.png",}, 
   ]
 
   return (
